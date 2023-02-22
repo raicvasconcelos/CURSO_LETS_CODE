a63108b02344f33aa38c0a854124f52043bf2bd6
F8371192 Railson da Conceicao Vasconcelos <railson.vasconcelos@bb.com.br>
2023-02-22 11:45:50 -0300

    projeto final modulo3 leitura csv completa

---
+++ Modulo3/ProjetoFinal/src/CSVReader.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVReader {
+
+  public static void main(String[] args) {
+    List<Pagamento> pagamentos = readPagamentosfromCSV(
+        "C:/Users/F8371192/OneDrive - Banco do Brasil S.A/Documentos/CURSO_LETS_CODE/Modulo3/ProjetoFinal/src/folhaPagamentos.CSV");
+        pagamentos.forEach(System.out::println);
+    }
+
+  private static List<Pagamento> readPagamentosfromCSV(String fileName) {
+    List<Pagamento> pagamentos = new ArrayList<>();
+    Path pathToFile = Paths.get(fileName);
+
+
+    try (
+      BufferedReader br = Files.newBufferedReader(
+        pathToFile,
+        StandardCharsets.US_ASCII
+      )
+    ) {
+      
+
+      br.readLine(); //Lê a primeira linha de cabeçalho
+      String line = br.readLine();
+      while ((line ) != null) {
+        String[] attributes = line.split(";");
+        Pagamento pagamento = createPagamento(attributes);
+        pagamentos.add(pagamento);
+        line = br.readLine();
+      }
+    } catch (IOException ioe) {
+      ioe.printStackTrace();
+    }
+
+    return pagamentos;
+  }
+
+  private static Pagamento createPagamento(String[] attributes) {
+ String clienteNome = attributes[0];
+ LocalDate dataVencimento = LocalDate.parse(attributes[1], DateTimeFormatter.ofPattern("dd/MM/yyyy")) ;
+ Double valor = Double.parseDouble( attributes[2]);
+ Integer classificacao = Integer.parseInt( attributes[3]);
+
+    return new Pagamento(clienteNome, dataVencimento, valor, classificacao);
+  }
+}
---
+++ Modulo3/ProjetoFinal/src/Pagamento.java
@@ -0,0 +1,52 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Pagamento {
+
+private String clienteNome;
+private LocalDate dataVencimento ;
+private Double valor;
+private Integer classificacao;
+
+public static final DateTimeFormatter formarterDate = DateTimeFormatter.ofPattern(
+    "dd/MM/yyyy"
+  );
+
+
+public Pagamento(String clienteNome, LocalDate dataVencimento, Double valor, Integer classificacao) {
+    this.clienteNome = clienteNome;
+    this.dataVencimento = dataVencimento;
+    this.valor = valor;
+    this.classificacao = classificacao;
+}
+
+
+public String getClienteNome() {
+    return clienteNome;
+}
+
+
+public LocalDate getDataVencimento() {
+    return dataVencimento;
+}
+
+
+public Double getValor() {
+    return valor;
+}
+
+
+public Integer getClassificacao() {
+    return classificacao;
+}
+
+
+@Override
+public String toString() {
+    return "Pagamento [clienteNome=" + clienteNome + ", dataVencimento=" + dataVencimento.format(formarterDate) + ", valor=" + valor
+            + ", classificacao=" + classificacao + "]";
+}
+
+
+    
+}
